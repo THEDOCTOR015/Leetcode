0798a77fc74173700e9dda1e8c8eaaebe16b0b65
THE_DOCTOR01 <91897252+THEDOCTOR015@users.noreply.github.com>
2023-09-23 01:00:42 +0200

    22/remix

---
+++ 22 (remix).py
@@ -0,0 +1,39 @@
+"""
+Answer :
+"""
+
+global listpackets
+listpackets = []
+class Solution:
+    def createpackets(depth,currentpacket,goal) :
+        print("currentpacket : ",currentpacket," depth : ",depth," goal : ",goal)
+        if depth == goal :
+            if currentpacket not in listpackets :
+                listpackets.append(currentpacket)
+            return
+        newpacket = currentpacket.copy()
+        newpacket.append(1)
+        Solution.createpackets(depth+1,newpacket,goal)
+        for i in range(len(currentpacket)) :
+            newpacket = currentpacket.copy()
+            newpacket[i] += 1
+            Solution.createpackets(depth+1,newpacket,goal)
+
+    def packetstoparenthesis(packets):
+        res = ""
+        for packet in packets :
+            res += "("*packet + ")"*packet
+        return res
+    
+    def generateParenthesis(self, n):
+        listparenthesis = []
+        Solution.createpackets(1,[1],n)
+        for packets in listpackets :
+            listparenthesis.append(Solution.packetstoparenthesis(packets))
+        return listparenthesis
+
+"""
+Tests :
+"""
+
+print(Solution.generateParenthesis(Solution,8))
