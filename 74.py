649133f0211da6b4cff02f4f2aa4fa3279fa7b5b
THE_DOCTOR01 <91897252+THEDOCTOR015@users.noreply.github.com>
2023-10-03 03:03:33 +0200

    Create 74.py

---
+++ 74.py
@@ -0,0 +1,24 @@
+"""
+Answer:
+"""
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            mid_row, mid_col = divmod(mid, n) # Permet de situer l'emplacement sur la matrice
+
+            if matrix[mid_row][mid_col] == target:
+                return True
+            elif matrix[mid_row][mid_col] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
+"""
+Tests:
+"""
