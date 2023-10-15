d0d2b526701d02c3c029f9d8735186bf1f42a16a
THE_DOCTOR01 <alexandre.luca23@gmail.com>
2023-10-15 18:32:38 +0200

    Update 146.py

--- 146.py
+++ 146.py
@@ -38,10 +38,63 @@ class LRUCache:
 Answer (double linked list):
 """
 class LRUCache:
+    class Node:
+        def __init__(self,key,val) :
+            self.key = key
+            self.val = val
+            self.pres = None
+            self.next = None
 
     def __init__(self, capacity):
+        self.capacity = capacity
+        self.map = {} # Key is key and value is Node
+        self.tail = self.Node("Tail","Tail")
+        self.head = self.Node("Head","Head")
+        self.tail.next = self.head
+        self.head.pres = self.tail
+
+    def addNode(self,newnode):
+        OldNewNode = self.head.pres
+
+        newnode.pres = OldNewNode
+        newnode.next = self.head
+        self.map[newnode.key] = newnode
+
+        OldNewNode.next = newnode
+        self.head.pres = newnode
+
+    def deleteNode(self,oldnode):
+        presNode = oldnode.pres
+        nextNode = oldnode.next
+        presNode.next = nextNode
+        nextNode.pres = presNode
+        del self.map[oldnode.key]
+
     def get(self, key):
+        if key not in self.map :
+            return -1
+        # On recup la valeur
+        Noderes = self.map[key]
+        res = Noderes.val
+
+        # On supprime le Node
+        self.deleteNode(Noderes)
+        # On ajoute le Node après Head
+        self.addNode(Noderes)
+        return res
+
     def put(self, key, value):
+        # Si le node existe, tu le supprime
+        if key in self.map :
+            self.deleteNode(self.map[key])
+        # Tu ajoute le nouveau node
+        NewNode = self.Node(key,value)
+        self.addNode(NewNode)
+        # Si trop d'element dans le cache
+        if len(self.map)-1 == self.capacity :
+            # on supprime le node le plus proche de tail
+            self.deleteNode(self.tail.next)
+
 
 """
 Tests:
