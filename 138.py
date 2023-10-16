fc0a8d755b67edf023d2b3b2e1dadbd98e1376d3
THE_DOCTOR01 <alexandre.luca23@gmail.com>
2023-10-16 13:38:06 +0200

    Create 138.py

---
+++ 138.py
@@ -0,0 +1,18 @@
+"""
+Answer:
+"""
+
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution:
+    def copyRandomList(self, head):
+        copy = 
+"""
+Tests:
+"""
